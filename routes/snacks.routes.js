4cb0252eefcaa710cc9cae9bf837d4371ab91f16
Kevin Springer <KevinUSCU@outlook.com>
2017-11-30 15:18:24 -0800

    Rename snacks.js to snacks.routes.js. Move base route url '/snacks' to server.js.

--- routes/snacks.routes.js
+++ routes/snacks.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const snacksController = require('../controllers/snacks.controller')
 
-router.get('/snacks', snacksController.showAll)
+router.get('/', snacksController.showAll)
 
 
 module.exports = router
