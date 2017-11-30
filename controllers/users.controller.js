abfda45deff2cf49ad7f6769f672cb9c7dd758f9
Kevin Springer <KevinUSCU@outlook.com>
2017-11-30 15:19:41 -0800

    Add users controller and model. Start to build functions.

---
+++ controllers/users.controller.js
@@ -0,0 +1,39 @@
+const UserModel = require('../models/user.model')
+
+class UsersController {
+  static index (req, res, next) {
+    UserModel.getAll()
+    .then(response => res.json({response}))
+    .catch(err => res.json(err))
+  }
+
+  static showOne (req, res, next) {
+
+  }
+
+  static create (req, res, next) {
+    const { first_name, last_name, email, password } = req.body
+    // need to verify all fields and no duplicate emails
+    UserModel.create(first_name, last_name, email, password)
+    .then(response => res.json({response}))
+    .catch(err => res.json(err))
+  }
+
+  static update (req, res, next) {
+
+  }
+
+  static destroy (req, res, next) {
+    
+  }
+
+  static login (req, res, next) {
+
+  }
+
+  static signup (req, res, next) {
+
+  }
+}
+
+module.exports = UsersController
---
+++ models/user.model.js
@@ -0,0 +1,53 @@
+const db = require('../db/knex')
+const bcrypt = require('bcryptjs')
+
+class UserModel {
+  static getAll () {
+    return db('users')
+    .select('id', 'first_name', 'last_name', 'email', 'role')
+  }
+
+  static getUser (id) {
+    return db('users')
+    .select('id', 'first_name', 'last_name', 'email', 'role')
+    .where({ id })
+    .first()
+  }
+
+  // create is the same as signing up a new user
+  static create (first_name, last_name, email, password) {
+    const hashed_password = bcrypt.hashSync(password)
+    return db('users')
+    .insert({ first_name, last_name, email, hashed_password })
+    .returning(['id', 'hashed_password'])
+  }
+
+  static update () {
+
+  }
+
+  static destroy (id) {
+
+  }
+
+  // requestToken is the equivalent of logging in
+  static requestToken (email, password) {
+
+  }
+
+  // this will verify a user
+  static verify (token) {
+
+  }
+}
+
+
+module.exports = UserModel
+
+
+function updateShroom(id, owner_id, name, cap, base, mouth, eyes, eyeballs, eyebrows, flourish, cap_color_1, cap_color_2) {
+  return knex('shrooms')
+  .where({ id })
+  .update({ owner_id, name, cap, base, mouth, eyes, eyeballs, eyebrows, flourish, cap_color_1, cap_color_2, thisKeyIsSkipped: undefined })
+  .returning(['name'])
+}
