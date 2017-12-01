2f568010e68650890b034916b5d6fb1b3a13b3ee
Kevin Springer <KevinUSCU@outlook.com>
2017-12-01 00:04:38 -0800

    Add comments. Remove signup method, since it will be handled by create.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,18 +2,21 @@ const UserModel = require('../models/user.model')
 
 class UsersController {
   static index (req, res, next) {
+    // should require admin token?
     UserModel.getAll()
     .then(response => res.json({response}))
     .catch(err => res.json(err))
   }
 
   static showOne (req, res, next) {
+    // should require admin or this user's token?
     UserModel.getUser(req.params.id)
     .then(response => res.json({response}))
     .catch(err => res.json(err))
   }
 
   static create (req, res, next) {
+    // create is the same as signing up a new user; no token required
     const { first_name, last_name, email, password } = req.body
     // need to verify all fields and no duplicate emails
     UserModel.create(first_name, last_name, email, password)
@@ -30,10 +33,6 @@ class UsersController {
   }
 
   static login (req, res, next) {
-
-  }
-
-  static signup (req, res, next) {
 
   }
 }
