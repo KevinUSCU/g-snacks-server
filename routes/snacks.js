8a9b71c0558b80b8b8e6ca6cfd443f1fcf7f29a9
Justin Hays <jhays124@yahoo.com>
2017-11-30 16:04:26 -0800

    route for getOneSnack functional

--- routes/snacks.js
+++ routes/snacks.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const snacksController = require('../controllers/snacks.controller')
 
 router.get('/snacks', snacksController.showAll)
+router.get('/snacks/:id', snacksController.getOne)
 
 
 module.exports = router
