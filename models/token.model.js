67929d245f97c15a92ef0955141370dd8eba7a03
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 00:59:05 -0800

    Improve token creation and extraction.

--- models/token.model.js
+++ models/token.model.js
@@ -5,13 +5,14 @@ const verifyPromise = promisify(verify)
 const secret = process.env.SECRET_KEY
 
 class Token {
-  static signAsync({ id, first_name, role }) {
-    const sub = { id, role }
-    const exp = '7 days'
+  static signAsync(id, first_name, last_name, role) {
+    const sub = { id, first_name, last_name, role }
+    const expiresIn = '7 days'
     return signPromise({ sub }, secret, { expiresIn })
   }
 
-  static parseAsync(token) {
+  static extractBearerToken(authHeader) {
+    const token = authHeader ? authHeader.replace('Bearer ', '') : null
     return verifyPromise(token, secret) //on success this returns the decoded token
   }
 }
