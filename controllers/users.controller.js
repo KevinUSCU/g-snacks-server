1a163d697d335fa04217ed6a0a0269728311dc66
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 01:00:09 -0800

    Add working signup and login routes. Start to enable auth checking in other functions.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,48 +1,113 @@
 const UserModel = require('../models/user.model')
+const Token = require('../models/token.model')
+const serverError = require('./errors.controller').serverError
+const bcrypt = require('bcryptjs')
 
 class UsersController {
   static index (req, res, next) {
-    // should require admin token?
-    UserModel.getAll()
-    .then(response => res.json({response}))
-    .catch(err => res.json(err))
+    // Require admin token to retrieve index of all users
+    // Get token from auth header
+    const authorization = req.headers.authorization
+    Token.extractBearerToken(authorization)
+    // This catch will grab errors from the token decoding process (such as no token or invalid token)
+    // .catch(err => next({ status: 401, message: 'A valid authorization token is required' }))
+    .then(token => {
+      if (token.sub.role !== 'admin') {
+        next({ status: 401, message: 'User not authorized for this resource' })
+        throw new Error()
+      }
+      return //pass auth check
+    })
+    // Get users data from database
+    .then(() => UserModel.getAll())
+    .then(users => res.status(200).json({ response: users }))
+    .catch(err => serverError(err, next))
   }
 
   static showOne (req, res, next) {
     // should require admin or this user's token?
-    UserModel.getUser(req.params.id)
-    .then(response => res.json({response}))
-    .catch(err => res.json(err))
+    const id = req.params.id
+    UserModel.getUser(id)
+    .then(user => {
+      if (!user) return next({ status: 404, message: `User ${id} does not exist` })
+      return res.json(user)
+    })
+    .catch(err => serverError(err, next))
   }
 
   static create (req, res, next) {
-    // create is the same as signing up a new user; no token required
+    // create is equivalent to signing up a new user; no token is required
     const { first_name, last_name, email, password } = req.body
     // verify all fields exist
-    if (!first_name) return next({ status: 400, error: 'First name is required' })
-    if (!last_name) return next({ status: 400, error: 'Last name is required' })
-    if (!email) return next({ status: 400, error: 'Email address is required' })
-    if (!password) return next({ status: 400, error: 'A password is required' })
+    if (!first_name) return next({ status: 400, message: 'First name is required' })
+    if (!last_name) return next({ status: 400, message: 'Last name is required' })
+    if (!email) return next({ status: 400, message: 'Email address is required' })
+    if (!password) return next({ status: 400, message: 'A password is required' })
     // verify email is unique
-    UserModel.getUserByEmail(email)
+    UserModel.getUserForVerification(email)
     .then(user => {
-      if (user) return next({ status: 409, error: 'A user with this email address already exists.' })
+      if (user) {
+        next({ status: 409, message: 'A user with this email address already exists.' })
+        throw new Error() //this will stop execution of the following .then code and be caught by serverError()
+      }
+      // add new user to database
       return UserModel.create(first_name, last_name, email, password)
     })
-    .then(() => UserModel.requestToken(email, password))
-    .then(token => res.json({ token }))
-    .catch(err => res.json(err))
+    // Sign and return token
+    .then(newUser => Token.signAsync(newUser[0].id, first_name, last_name, newUser[0].role))
+    .then(token => res.status(201).json({ token }))
+    .catch(err => serverError(err, next))
   }
 
   static update (req, res, next) {
-
+    const id = req.params.id
+    const { first_name, last_name, email, password, role } = req.body
+    UserModel.update(id, first_name, last_name, email, password, role)
+    .then(userId => {
+      if (userId[0]) return res.status(200).json(userId[0].id)
+      return next({ status: 404, message: 'User does not exist.' })
+    })
   }
 
   static destroy (req, res, next) {
-    
+    const id = req.params.id
+    return UserModel.destroy(id)
+    .then(userId => {
+      if (!userId) return next({ status: 404, message: 'This user did not exist.' })
+      return res.status(204)
+    })
   }
 
   static login (req, res, next) {
+    const { email, password } = req.body
+    UserModel.getUserForVerification(email)
+    .then(user => {
+      if (!user) {
+        next({ status: 404, message: 'User does not exist' })
+        throw new Error()
+      }
+      if (!password) {
+        next({ status: 400, message: 'Password is required' })
+        throw new Error()
+      }
+      if (!bcrypt.compareSync(password, user.hashed_password)) {
+        next({ status: 401, message: 'Incorrect password' })
+        throw new Error()
+      }
+      return user
+    })
+    .then(user => {
+      const { id, first_name, last_name, role } = user
+      return Token.signAsync(id, first_name, last_name, role)
+    })
+    .then(token => res.status(201).json({ token }))
+    .catch(err => serverError(err, next))
+  }
+
+  
+
+  // this will verify a user
+  static verify (token) {
 
   }
 }
--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,5 @@
 const db = require('../db/knex')
-const { promisify } = require('util')
-const { hash, compare } = require('bcryptjs')
-const hashPromise = promisify(hash)
-const comparePromise = promisify(compare)
-const Token = require('./token.model')
+const bcrypt = require('bcryptjs')
 
 class UserModel {
   static getAll () {
@@ -18,68 +14,37 @@ class UserModel {
     .first()
   }
 
-  static getUserByEmail(email) {
+  static getUserForVerification(email) {
     return db('users')
-    .select('email')
+    .select('id', 'first_name', 'last_name', 'role', 'hashed_password')
     .where({ email })
     .first()
   }
 
   static create (first_name, last_name, email, password) { 
-    return hashPromise(password)
-    .then(hashed_password => {
-      return db('users')
-      .insert({ first_name, last_name, email, hashed_password })
-      // note that 'role' is automatically defaulted to 'user' by the db
-      .returning(['id'])
-    })
-    .catch(error => { throw error })
+    const hashed_password = bcrypt.hashSync(password)
+    return db('users')
+    .insert({ first_name, last_name, email, hashed_password })
+    // note that 'role' is automatically defaulted to 'user' by the db
+    .returning(['id', 'role'])
   }
 
   static update (id, first_name, last_name, email, password, role) {
-    if (password) {
-      return hashPromise(password)
-      .then(hashed_password => {
-        return db('users')
-        .where({ id })
-        .update({ first_name, last_name, email, hashed_password, role, thisKeyIsSkipped: undefined })
-        .returning(['id'])
-      })
-      .catch(error => { throw error })
-    } else {
-      return db('users')
+    let hashed_password
+    if (password) hashed_password = bcrypt.hashSync(password)
+    return db('users')
       .where({ id })
-      .update({ first_name, last_name, email, role, thisKeyIsSkipped: undefined })
+      .update({ first_name, last_name, email, hashed_password, role, thisKeyIsSkipped: undefined })
       .returning(['id'])
-    }
   }
 
   static destroy (id) {
     return db('users')
     .where({ id })
-    .del()
     .returning(['id'])
+    .del()
   }
 
-  // requestToken is the equivalent of logging in
-  static requestToken (email, password) {
-    return db('users')
-    .select('id', 'first_name', 'last_name', 'hashed_password')
-    .where({ email })
-    .first()
-    .then(user => {
-      if (!user) throw new Error('No such user')
-      if (!bcrypt.compareSync(password, user.hashed_password)) throw new Error('Incorrect password')
-      return user
-    })
-    .then(user => Token.signAsync(user))
-    .catch(error => { throw error })
-  }
-
-  // this will verify a user
-  static verify (token) {
-
-  }
 }
 
 
