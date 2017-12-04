6782d9f527b19845896c6d0c198fdd68051db21f
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 19:16:20 -0800

    Update method names. Update verify to parse the header.

--- models/token.model.js
+++ models/token.model.js
@@ -5,14 +5,16 @@ const verifyPromise = promisify(verify)
 const secret = process.env.SECRET_KEY
 
 class Token {
-  static signAsync(id, first_name, last_name, role) {
+  // Both of these Token methods are async and return a promise
+
+  static sign(id, first_name, last_name, role) {
     const sub = { id, first_name, last_name, role }
     const expiresIn = '7 days'
     return signPromise({ sub }, secret, { expiresIn })
   }
 
-  static extractBearerToken(authHeader) {
-    const token = authHeader ? authHeader.replace('Bearer ', '') : null
+  static verifyAndExtractHeaderToken(header) {
+    const token = header.authorization ? header.authorization.replace('Bearer ', '') : null
     return verifyPromise(token, secret) //on success this returns the decoded token
   }
 }
