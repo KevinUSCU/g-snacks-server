a15205cffbba8fbed1345c14faf427ad41d2615a
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 19:15:11 -0800

    Update comment.

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,5 +1,5 @@
 function serverError(err, next) {
-  /* This prints errors to the server, and sends a generic message to the client. This will surface things like database errors which may include details which we don't want the client to have details of for security reasons. We check for err.message to ignore errors with empty messages, since we use those to stop propagation of chained promises. */  
+  /* This prints errors to the server while sending a generic message to the client. This will surface things like database errors which may include details which we don't want the client to have details of for security reasons. We check for err.message to ignore errors with empty messages as we will use those to stop propagation of chained promises. */  
   if (err.message) {
     console.log(err)
     return next({ status: 500, message: 'Check server logs for error details' })
