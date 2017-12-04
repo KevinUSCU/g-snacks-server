e0e8ef98e0cfe3db5f1e3897c53c16bf1e23f53c
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 23:26:49 -0800

    Add destroy and changeRole methods. Update all methods to verify existence of user described in token and to grab user details from db instead of token. Also verify that duplicate emails will not be created by update method.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,6 @@
 const UserModel = require('../models/user.model')
 const Token = require('../models/token.model')
-const serverError = require('./errors.controller').serverError
+const { serverError, ThrowError } = require('./errors.controller')
 const bcrypt = require('bcryptjs')
 
 class UsersController {
@@ -9,20 +9,16 @@ class UsersController {
     // *** Require admin token to retrieve index of all users ***
     // Validate and decode token
     Token.verifyAndExtractHeaderToken(req.headers)
-    .catch(err => {
-      // This catch will grab errors from the token decoding process (such as no token or invalid token)
-      next({ status: 401, message: 'A valid authorization token is required' })
-      throw new Error() //this will stop execution of the following .then code and be caught by serverError()
+    .catch(err => ThrowError.invalidToken(next))
+    // Check for and retrieve user from database
+    .then(token => UserModel.getUser(token.sub.id))
+    // Verify user
+    .then(user => {
+      if (!user) return ThrowError.requestorInvalid(next)
+      if (user.role !== 'admin') return ThrowError.unauthorizedUser(next)
+      // Pass auth check; get users from db
+      return UserModel.getAll()
     })
-    .then(token => {
-      if (token.sub.role !== 'admin') {
-        next({ status: 401, message: 'User is not authorized for this resource' })
-        throw new Error()
-      }
-      return //pass auth check
-    })
-    // Get users data from database
-    .then(() => UserModel.getAll())
     .then(users => res.status(200).json({ response: users }))
     .catch(err => serverError(err, next))
   }
@@ -32,22 +28,18 @@ class UsersController {
     const id = req.params.id
     // Validate and decode token
     Token.verifyAndExtractHeaderToken(req.headers)
-    .catch(err => {
-      // This catch will grab errors from the token decoding process (such as no token or invalid token)
-      next({ status: 401, message: 'A valid authorization token is required' })
-      throw new Error() //this will stop execution of the following .then code and be caught by serverError()
-    })
-    .then(token => {
-      if (!(token.sub.role === 'admin' || (token.sub.role === 'user' && token.sub.id == id))) {
-        next({ status: 401, message: 'User is not authorized for this resource' })
-        throw new Error()
-      }
-      return //pass auth check
-    })
-    // Get user data from database
-    .then(() => UserModel.getUser(id))
+    .catch(err => ThrowError.invalidToken(next))
+    // Check for and retrieve user from database
+    .then(token => UserModel.getUser(token.sub.id))
+    // Verify user
     .then(user => {
-      if (!user) return next({ status: 404, message: `User does not exist` })
+      if (!user) return ThrowError.requestorInvalid(next)
+      if (!(user.role === 'admin' || (user.role === 'user' && user.id == id))) ThrowError.unauthorizedUser(next)
+      // Pass auth check; get user data from db
+      return UserModel.getUser(id)
+    })
+    .then(user => {
+      if (!user) return ThrowError.noSuchUser(next)
       return res.status(200).json({ response: user })
     })
     .catch(err => serverError(err, next))
@@ -62,17 +54,14 @@ class UsersController {
     if (!email) return next({ status: 400, message: 'Email address is required' })
     if (!password) return next({ status: 400, message: 'A password is required' })
     // Verify that email is unique
-    UserModel.getUserForVerification(email)
-    .then(user => {
-      if (user) {
-        next({ status: 409, message: 'A user with this email address already exists.' })
-        throw new Error() //this will stop execution of the following .then code and be caught by serverError()
-      }
+    UserModel.getUserIdByEmail(email)
+    .then(existingUser => {
+      if (existingUser) return ThrowError.duplicateUser(next)
       // If unique, add new user to database; all new users created with role of 'user'
       return UserModel.create(first_name, last_name, email, password)
     })
     // Sign and return a token for the new user
-    .then(newUser => Token.sign(newUser[0].id, first_name, last_name, newUser[0].role))
+    .then(newUserId => Token.sign(newUserId[0].id))
     // Return token to client
     .then(token => res.status(201).json({ response: token }))
     .catch(err => serverError(err, next))
@@ -81,44 +70,48 @@ class UsersController {
   static update (req, res, next) {
     // *** Editing profile data requires the user who's id we are updating ***
     const id = req.params.id
-    const { first_name, last_name, email, password, role } = req.body
+    const { first_name, last_name, email, password } = req.body
     // Validate and decode token
     Token.verifyAndExtractHeaderToken(req.headers)
-    .catch(err => {
-      // This catch will grab errors from the token decoding process (such as no token or invalid token)
-      next({ status: 401, message: 'A valid authorization token is required' })
-      throw new Error() //this will stop execution of the following .then code and be caught by serverError()
-    })
-    .then(token => {
-      if (token.sub.id != id) {
-        next({ status: 401, message: 'User is not authorized for this resource' })
-        throw new Error()
-      }
-      return //pass auth check
+    .catch(err => ThrowError.invalidToken(next))
+    // Check for and retrieve user from database
+    .then(token => UserModel.getUser(token.sub.id))
+    // Verify User
+    .then(user => {
+      if (!user) return ThrowError.requestorInvalid(next)
+      if (user.id != id) return ThrowError.unauthorizedUser(next)
+      // Pass auth check
+      // If email was changed, verify no duplicates
+      if (email) {
+        return UserrModel.getUserIdByEmail(email)
+        .then(existingUser => {
+          if (existingUser) return ThrowError.duplicateUser(next)
+        })
+      } else return
     })
     // Update user profile with supplied data
-    .then(() => UserModel.update(id, first_name, last_name, email, password, role))
-    // Sign and return updated token
-    .then(user => {
-      if (!user) {
-        next ({ status: 404, message: 'User does not exist.' })
-        throw new Error()
-      }
-      const { id, first_name, last_name, role } = user[0]
-      return Token.sign(id, first_name, last_name, role)
-    })
-    // Return token to client
-    .then(token => res.status(201).json({ response: token }))
+    .then(() => UserModel.update(id, first_name, last_name, email, password))
+    .then(userId => res.status(200).json({ response: userId }))
     .catch(err => serverError(err, next))
   }
 
   static destroy (req, res, next) {
+    // *** Deleting a user requires either role of 'admin' or the user who's id we are requesting ***
     const id = req.params.id
-    return UserModel.destroy(id)
-    .then(userId => {
-      if (!userId) return next({ status: 404, message: 'This user did not exist.' })
-      return res.status(204)
+    // Validate and decode token
+    Token.verifyAndExtractHeaderToken(req.headers)
+    .catch(err => ThrowError.invalidToken(next))
+    // Check for and retrieve user from database
+    .then(token => UserModel.getUser(token.sub.id))
+    // Verify user
+    .then(user => {
+      if (!user) return ThrowError.requestorInvalid(next)
+      if (!(user.role === 'admin' || (user.role === 'user' && user.id == id))) ThrowError.unauthorizedUser(next)
+      //pass auth check; delete user
+      return UserModel.destroy(id)
     })
+    .then(response => res.status(204).json())
+    .catch(err => serverError(err, next))
   }
 
   static login (req, res, next) {
@@ -127,35 +120,43 @@ class UsersController {
     const { email, password } = req.body
     if (!email) return next({ status: 400, message: 'Email address is required'})
     if (!password) return next({ status: 401, message: 'Password is required' })
+    // Retrieve user match from database
     UserModel.getUserForVerification(email)
     .then(user => {
-      if (!user) {
-        next({ status: 404, message: 'User does not exist' })
-        throw new Error()
-      }
+      if (!user) return ThrowError.noSuchUser(next)
       // Check for supplied password match against stored hash
-      if (!bcrypt.compareSync(password, user.hashed_password)) {
-        next({ status: 401, message: 'Incorrect password' })
-        throw new Error()
-      }
-      // Pass user data to token signing step
-      return user
-    })
-    .then(user => {
-      // Sign new token with user data
-      const { id, first_name, last_name, role } = user
-      return Token.sign(id, first_name, last_name, role)
+      if (!bcrypt.compareSync(password, user.hashed_password)) return ThrowError.invalidPassword(next)
+      // Sign new token with user id
+      return Token.sign(user.id)
     })
     // Return token to client
     .then(token => res.status(201).json({ response: token }))
     .catch(err => serverError(err, next))
   }
 
-  
-
-  // this will verify a user
-  static verify (token) {
-
+  static changeRole (req, res, next) {
+    // An 'admin' token is required to change the role of another user
+    const id = req.params.id
+    const role = req.body.role
+    if (!role) return next({ status: 400, message: 'Role attribute is required'})
+    if (role !== 'admin' && role !== 'user') return next({ status: 400, message: "Role attribute must be either 'admin' or 'user'" })
+    // Validate and decode token
+    Token.verifyAndExtractHeaderToken(req.headers)
+    .catch(err => ThrowError.invalidToken(next))
+    // Check for and retrieve user from database
+    .then(token => UserModel.getUser(token.sub.id))
+    // Verify user
+    .then(user => {
+      if (!user) return ThrowError.requestorInvalid(next)
+      if (user.role !== 'admin') return ThrowError.unauthorizedUser(next)
+      // Pass auth check; update role of user in db
+      return UserModel.update(id, undefined, undefined, undefined, undefined, role)
+    })
+    .then(userId => {
+      if (!userId) return ThrowError.noSuchUser(next)
+      return res.status(200).json({ response: userId })
+    })
+    .catch(err => serverError(err, next))
   }
 }
 
