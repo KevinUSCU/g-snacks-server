8230babcdb1f978ceb542b410f51682b12fa8190
Kevin Springer <KevinUSCU@outlook.com>
2017-12-03 23:28:31 -0800

    Fix bug in destroy. Modify getUserForVerification to only return id and hashed_password. Add getUserIdByEmail to verify existence of users. Create and update now only return 'id'.

--- models/user.model.js
+++ models/user.model.js
@@ -14,9 +14,16 @@ class UserModel {
     .first()
   }
 
-  static getUserForVerification(email) {
+  static getUserIdByEmail (email) {
     return db('users')
-    .select('id', 'first_name', 'last_name', 'role', 'hashed_password')
+    .select('id')
+    .where({ email })
+    .first()
+  }
+
+  static getUserForVerification (email) {
+    return db('users')
+    .select('id', 'hashed_password')
     .where({ email })
     .first()
   }
@@ -26,7 +33,7 @@ class UserModel {
     return db('users')
     .insert({ first_name, last_name, email, hashed_password })
     // note that 'role' is automatically defaulted to 'user' by the db
-    .returning(['id', 'role'])
+    .returning(['id'])
   }
 
   static update (id, first_name, last_name, email, password, role) {
@@ -35,13 +42,12 @@ class UserModel {
     return db('users')
       .where({ id })
       .update({ first_name, last_name, email, hashed_password, role, thisKeyIsSkipped: undefined })
-      .returning(['id', 'first_name', 'last_name', 'role'])
+      .returning(['id'])
   }
 
   static destroy (id) {
     return db('users')
     .where({ id })
-    .returning(['id'])
     .del()
   }
 
