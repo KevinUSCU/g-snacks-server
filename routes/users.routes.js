1b93ba4c8eeb15afde8d8a522fe696bcdcfb0de0
Kevin Springer <KevinUSCU@outlook.com>
2017-12-04 14:19:32 -0800

    Organize routes with comments. Add get fromToken route.

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,13 +2,25 @@ const express = require('express')
 const router = express.Router()
 const UsersCtrl = require('../controllers/users.controller')
 
-router.post('/login', UsersCtrl.login)
+// Authentication
 router.post('/signup', UsersCtrl.create) // new users are only created through signup
+router.post('/login', UsersCtrl.login)
+
+// Edit user
+// Change user profile
+router.put('/:id', UsersCtrl.update)
+// Change user role
 router.post('/promote/:id', UsersCtrl.changeRole)
 
+// View users
+// View user by token
+router.get('/fromToken', UsersCtrl.showOneFromToken)
+// View all users
 router.get('/', UsersCtrl.index)
+// View user by id route (might not be needed?)
 router.get('/:id', UsersCtrl.showOne)
-router.put('/:id', UsersCtrl.update)
+
+// Delete User
 router.delete('/:id', UsersCtrl.destroy)
 
 module.exports = router
