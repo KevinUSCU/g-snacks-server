1558a7585ca8664f0cfca862b8ba5655d3bd9ce4
Kevin Springer <KevinUSCU@outlook.com>
2017-12-06 16:52:19 -0800

    Restore getOne snack route & change route for getOneWithReviews to be of the format /:id/reviews.

--- routes/snacks.routes.js
+++ routes/snacks.routes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const snacksController = require('../controllers/snacks.controller')
 
 router.get('/', snacksController.showAll)
-router.get('/:id', snacksController.getOneWithReviews)
+router.get('/:id', snacksController.getOne)
+router.get('/:id/reviews', snacksController.getOneWithReviews)
 router.put('/:id', snacksController.update)
 router.delete('/:id', snacksController.destroy)
 
