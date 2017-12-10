7bbc5d804e6f455948bbb14f76afe8f15648119c
Kevin Springer <KevinUSCU@outlook.com>
2017-12-10 09:44:01 -0800

    Updated seeds with data useful for demo.

--- db/seeds/2_users.js
+++ db/seeds/2_users.js
@@ -4,7 +4,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').insert([
     { id: 1, first_name: 'Ella', last_name: 'Fitzgerald', email: 'ella.fitz@gmail.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' },
     { id: 2, first_name: 'Marco', last_name: 'Polo', email: 'mp@gmail.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa', role: 'admin' },
-    { id: 3, first_name: 'Scooby', last_name: 'Doo', email: 's.snacks@yahoo.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' }
+    { id: 3, first_name: 'Scooby', last_name: 'Doo', email: 's.snacks@yahoo.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' },
+    { id: 4, first_name: 'Cookie', last_name: 'Monster', email: 'user@gmail.com', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' },
+    { id: 5, first_name: 'Jabba', last_name: 'The Hutt', email: 'admin@gmail.com', role: 'admin', hashed_password: '$2a$10$x5BXBKwhjo.VCl8A/tuFfuLfEv8narDxPbvx6yMHlG.Y6EreIcCFa' }
   ])
   .then(() => {
     return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`)
--- db/seeds/3_reviews.js
+++ db/seeds/3_reviews.js
@@ -20,7 +20,7 @@ exports.seed = function(knex, Promise) {
     {id: 17, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 17, user_id: 3},
     {id: 18, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 18, user_id: 3},
     {id: 19, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 19, user_id: 3},
-    {id: 20, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 20, user_id: 3},
+    {id: 20, title: 'Double wow!', text: 'These *are* delicous!', rating: 3, snack_id: 19, user_id: 1},
     {id: 21, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 21, user_id: 3},
     {id: 22, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 22, user_id: 3},
     {id: 23, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 23, user_id: 3},
@@ -31,7 +31,13 @@ exports.seed = function(knex, Promise) {
     {id: 28, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 28, user_id: 3},
     {id: 29, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 29, user_id: 3},
     {id: 30, title: 'Wow!', text: 'These are delicous!', rating: 3, snack_id: 3, user_id: 3},
-    {id: 31, title: '*Snobby Voice*', text: 'I have certainly had sooooo much better.', rating: 1, snack_id: 2, user_id: 2}
+    {id: 31, title: '*Snobby Voice*', text: 'I have certainly had sooooo much better.', rating: 1, snack_id: 2, user_id: 2},
+    {id: 32, title: "I can't have just one!", text: 'I love these! Not only are they yummy, but they give me that certain je ne sais quoi I need in the courage department to fight ghosts.', rating: 5, snack_id: 31, user_id: 3},
+    {id: 33, title: 'Bad for business', text: 'My guards love these, but you try keeping the larder full when all of your Gamorians have the munchies!', rating: 1, snack_id: 31, user_id: 5},
+    {id: 34, title: 'Biscotti Bliss!', text: 'Cookie! Me want cookie!', rating: 4, snack_id: 31, user_id: 4},
+    {id: 35, title: 'The treat that travels well', text: "I like 'em, and they are great at sea; they don't even need to be salt-packed!", rating: 4, snack_id: 31, user_id: 2},
+    {id: 36, title: 'Seriously? Yuck', text: 'I have certainly had sooooo much better.', rating: 1, snack_id: 31, user_id: 1},
+    {id: 37, title: 'Solo! Solo! Too Nakma Noya Solo!', text: 'Han ma boo-kee, keelee ka-lya dooka. Wadja da boolya ra Moy. Han, Mah kee cheezay. Hassatamooma koh kee malyaloongee.', rating: 5, snack_id: 32, user_id: 5}
   ])
   .then(() => {
     return knex.raw(`SELECT setval('reviews_id_seq', (SELECT MAX(id) FROM reviews));`)
